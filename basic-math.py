f74d9625d99419931decf1a0879bb788b8384dae
Paulshapwala <paulokuyo80@gmail.com>
2023-03-23 02:36:42 +0300

    [using input]

---
+++ basic-math.py
@@ -0,0 +1,7 @@
+print(19 + 5)
+a = 12
+b = 9
+print(a + b)
+c = 5.6
+d = 2.2
+print(c - d)
---
+++ sum.py
@@ -0,0 +1,4 @@
+a = float(input('enter a number: '))
+b = float(input('enter another number: '))
+# print('The sum = &d' %sum)
+print(f'The sum = {a + b}')
