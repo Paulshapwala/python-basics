5667bd2430653b7b91d88ec2788d154c9b209c48
Paulshapwala <paulokuyo80@gmail.com>
2023-03-24 10:26:23 +0300

    more features

--- intro_to_python.py
+++ intro_to_python.py
@@ -1,1 +1,12 @@
 print("let's get started")
+a = input('Enter an input')
+list = [1, 2, 4, 5]
+dict_item = {'1':1,
+             '2':2,
+             '3':3,
+             '4':4
+             
+             }
+dict_item['5'] = 5
+print(dict_item)
+print(f'your input was {a}')
